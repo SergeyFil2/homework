71805700ca92d0eee171783a8d33fb4fdc385f05
SergeyFil2 <search.2002@mail.ru>
2022-10-04 20:38:54 +0500

    my first h/w

---
+++ class.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+class Person:
+    def __init__(self, name, age, address, gender):
+        self.name, self.age, self.address, self.gender = name, age, address, gender
+        self.key = (name, address)
+
+    def __repr__(self):
+        return "Person('%s', '%s', '%s', '%s')" % (self.name, self.age, self.address)
+
+
+sergey = Person("Сергей", 20, "Ленина, 26, 69")
+
+people = {
+    sergey.key: sergey,
+}
+pprint(people)
+pprint(people[sergey.key])
