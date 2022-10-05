8fb266be072ccc25409b91615d641ad872258be6
SergeyFil2 <search.2002@mail.ru>
2022-10-05 16:57:01 +0500

    Задание по классам

--- class.py
+++ class.py
@@ -1,14 +1,14 @@
 from pprint import pprint
 class Person:
-    def __init__(self, name, age, address, gender):
-        self.name, self.age, self.address, self.gender = name, age, address, gender
-        self.key = (name, address)
+    def __init__(self, name, age, address, gender, height, weight):
+        self.name, self.age, self.address, self.gender, self.height, self.weight = name, age, address, gender, height, weight
+        self.key = (name, address, gender)
 
     def __repr__(self):
-        return "Person('%s', '%s', '%s', '%s')" % (self.name, self.age, self.address, self.gender)
+        return "Person('%s', %s, '%s', '%s', %s, %s)" % (self.name, self.age, self.address, self.gender, self.height, self.weight)
 
 
-sergey = Person("Сергей", 20, "Ленина, 26, 69", 'M')
+sergey = Person("Сергей", 20, "Ленина, 26, 69", 'Mужской', 180, 66)
 
 people = {
     sergey.key: sergey,
