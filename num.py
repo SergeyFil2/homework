f204acf8bb30c113d0c366462e59a8427dba35ba
SergeyFil2 <search.2002@mail.ru>
2022-10-05 17:03:23 +0500

    Задание numpy

--- num.py
+++ num.py
@@ -3,11 +3,12 @@ x=array(sum(range(100)))
 print ("сумма ряда 1-100 =", x)
 
 from numpy import array
+print('Введите целое число')
 y=int(input())
 x=array(sum(range(y)))
-print("сумма ряда 1-input()", x)
+print("Сумма ряда 1-input()", x)
 
 import numpy as np
-x=np.random.randint(10000, size=100)
+x=np.random.randint(1000, size=100)
 print(x)
-print("среднее среди 100 случайных чисел:", np.mean(x))
+print("Среднее среди 100 случайных чисел:", np.mean(x))
