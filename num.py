7bf1e9f811675648aa424fa284ce564552628a3d
SergeyFil2 <search.2002@mail.ru>
2022-10-05 17:07:30 +0500

    Задание numpy

--- num.py
+++ num.py
@@ -6,7 +6,7 @@ from numpy import array
 print('Введите целое число')
 y=int(input())
 x=array(sum(range(y)))
-print("Сумма ряда 1-input()", x)
+print("Сумма ряда 1-",y, 'равна', x)
 
 import numpy as np
 x=np.random.randint(1000, size=100)
