c467dbc0ffa06595ad796e2c282d54a7e8573d66
SergeyFil2 <search.2002@mail.ru>
2022-10-09 14:39:50 +0500

    Программа, отвечающая на любой вопрос да или нет

--- gad.py
+++ gad.py
@@ -2,7 +2,7 @@ from pprint import pprint
 from random import randint
 answers=("да","нет")
 question=""
-while question != "хватит":
+while question != "До свидания":
     question=input()
     answer=answers[randint(0, len(answers)-1)]
     pprint(answer)
