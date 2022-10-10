9da95654065619d3917020d056531f240ab6b117
SergeyFil2 <search.2002@mail.ru>
2022-10-10 14:36:15 +0500

    Программа,отвечающая на вопрос да, нет или не знаю

--- gad.py
+++ gad.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 from random import randint
-answers=("да","нет")
+answers=("Да","Нет", "Не знаю")
 question=""
-while question != "До свидания":
+while question != "До свидания" or "Хватит":
     question=input()
     answer=answers[randint(0, len(answers)-1)]
     pprint(answer)
